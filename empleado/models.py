bfaefc1e402ce3415eac1a019d8d1ef056af314e
sergio.itp16@gmail.com <sergio.itp16@gmail.com>
2013-10-24 18:07:13 -0500

    Agregado modelos de la aplicación empleado

--- empleado/models.py
+++ empleado/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from publico.models import Persona
 
-# Create your models here.
+class TipoContrato(models.Model):
+    idTipo= models.AutoField(primary_key=True)
+    nombre= models.CharField(max_length=100, null=False)
+    def __unicode__(self):
+            return self.nombre
+
+class Contrato(models.Model):
+    idContrato= models.AutoField(primary_key=True)
+    contratoFile= models.FileField(upload_to="docs/", null=False)
+    tipo= models.ForeignKey(TipoContrato)
+    empleado= models.ForeignKey(Persona)
+    fechaInicio= models.DateField(auto_now=False, null=False)
+    fechaFinal= models.DateField(auto_now=False)
+    def __unicode__(self):
+            return self.tipo
+class Rol(models.Model):
+    idRol= models.AutoField(primary_key=True)
+    nombreRol= models.CharField(max_length=100, null=False)
+    def __unicode__(self):
+            return self.nombreRol
+
+class Cargo(models.Model):
+    nombreCargo= models.CharField(primary_key=True, max_length=50, null=False)
+    descripcion= models.CharField(max_length=150)
+    salario= models.FloatField(null=False)
+    tipo= models.ForeignKey(Rol)
+    def __unicode__(self):
+        return self.nombreCargo
+
+class EmpleadoCargo(models.Model):
+    fechaInicio= models.DateField(auto_now=False, null=False)
+    fechaFinal= models.DateField(auto_now=False, null=False)
+    cargo= models.ForeignKey(Cargo)
+    persona= models.ForeignKey(Persona)
+    def __unicode__(self):
+            return self.cargo
