23edd79932ba493568417aa689d9f2cf8b74299e
sergio.itp16@gmail.com <sergio.itp16@gmail.com>
2013-10-25 18:32:54 -0500

    Modificación de la aplicación empleado
    Modificación de vista y plantilla de la aplicación empleado

--- empleado/views.py
+++ empleado/views.py
@@ -1,5 +1,17 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-
+from publico.models import Persona
 def lista_empleados(request):
-	return render_to_response('lista_empleados.html', context_instance=RequestContext(request))
+	personas = Persona.objects().all()
+	paginador = Paginator(personas, 10)
+	try:
+		pagina = request.GET['pagina']
+		personas = paginador.page(pagina)	
+	except PageNotAnInteger:
+		personas = paginador.page(1)
+	except EmptyPage:
+		personas = paginador.page(paginador.num_pages)
+	except Exception:
+		personas = paginador.page(1)
+	return render_to_response('lista_empleados.html',{'personas':personas}, context_instance=RequestContext(request))
